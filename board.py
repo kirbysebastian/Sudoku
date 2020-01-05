985eabf2942d70e1813a38de69c2dbc151a0271b
kirbysebastian <kirbypaolo.sebastian@gmail.com>
2020-01-05 11:58:05 +0800

    Fix board coordinates and loading sudoku puzzle

--- board.py
+++ board.py
@@ -7,10 +7,37 @@ class Board:
         return self.make_printable()
 
     def make_printable(self):
-        c_gap = ' | '
-        r_gap = '------------------'
-        return ''
+        # Fix variable names. // Uncle Bob rules XD
+        print('    A   B   C   D   E   F   G   H   I')
+        c_gap = ' : '
+        cx_gap = ' | '
+        r_gap = '   ----------- ----------- ----------- '
+        rx_gap = '   =========== =========== =========== ' 
+        string_table = rx_gap + '\n'
+        row_label = 1
+        for row_index, row in enumerate(self.board):
+            string_table += ' ' + str(row_label) + cx_gap[1:]
+            for e_index, elem in enumerate(row):
+                if e_index == 2 or e_index == 5 or e_index == 8:
+                    string_table += (str(elem) + cx_gap)
+                else:
+                    string_table += (str(elem) + c_gap)
 
-    def load(self):
-        # Load sudoku puzzle to board
-        pass
+            norm_gap = '\n' + r_gap + '\n'
+            sep_gap = '\n' + rx_gap + '\n'
+            if row_index == 2 or row_index == 5 or row_index == 8:
+                string_table += sep_gap
+            else:
+                string_table += norm_gap
+
+            row_label += 1
+        return string_table 
+
+    def load(self, puzzle: str):
+        # Load existing sudoku puzzle to board
+        self.board = [[data if data != '0' else ' ' for data in puzzle[i*9:(i*9)+9]] for i in range(9)]
+
+b = Board()
+b.load('005910308009403060027500100030000201000820007006007004000080000640150700890000420')
+print(b)
+
